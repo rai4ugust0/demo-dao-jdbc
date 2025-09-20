4a5fe47e00b8f935d79f8830e7af57bb08e92d2e
Raí Augusto <raiaugusto436@gmail.com>
2025-09-20 12:27:44 -0300

    DepartmentDao complete implementation and test

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Seller implements Serializable{
-    private Integer id;
+    private static final long serialVersionUID = 1L;
+	private Integer id;
     private String name;
     private String email;
     private Date birthDate;
